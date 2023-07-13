debd9de337bafc27e77918375a9b6c17091d2cb1
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-07-13 19:37:36 +0000

    to the standard error. (no printf family or puts)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1
+ **/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(str), 1, stderr);
+	return (EXIT_FAILURE);
+}
