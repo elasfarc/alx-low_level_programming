d063c803765026da0f9959c3adae746f78be4b27
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-07-19 02:20:27 +0000

    a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isLower - checks for lowercase char
+ * @c: the char to be checked
+ *
+ * Return: 1 if c is lowercase
+ *	   0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
