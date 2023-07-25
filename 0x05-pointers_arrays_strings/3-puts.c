3488a0363814fb67a8d5e42cb1facd4b611b21d1
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-07-25 03:00:06 +0000

    void _puts(char *str)

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline to std output
+ * @str: string to be pritnted
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 void reset_to_98(int *);
 void swap_int(int *a, int *b);
 int _strlen(char *);
+void _putchar(int);
+void _puts(char *)
