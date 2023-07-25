fde5961b1c5e38571e95dbd18c3c1b88007013d3
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-07-25 02:18:18 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
- * reset_to_98 - takes a pointer to an int 
- * 		 updates the value to 98
- * @*n: pointer to an int 
+ * reset_to_98 - takes a pointer to an int
+ *		 updates the value to 98
+ * @n: pointer to an int
  *
- * Return: void 
+ * Return: void
  */
 void reset_to_98(int *n)
 {
