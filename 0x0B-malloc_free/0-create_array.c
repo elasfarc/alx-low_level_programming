9ad80af7db7e8438add36c1158ef69f691ecdea1
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-08 14:48:47 +0000

    handle malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 {
 	return (size == 0
 		? NULL
-		: init_char_arr(malloc(size * sizeof(char)), size, c));
+		: init_char_arr((char *) malloc(size * sizeof(char)), size, c));
 }
 
 /**
