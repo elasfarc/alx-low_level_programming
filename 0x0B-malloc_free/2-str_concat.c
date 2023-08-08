825a2a9faab6d4d127bc4280dab68e288e62d1b6
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-08 19:48:47 +0000

    str_concat(..)

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+short should_loop(char *s, short i);
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Null on failure or a pointer points to the newly concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned short int len1 = 0, len2 = 0, len3, i, j;
+	short is_s1 = should_loop(s1, len1);
+	short is_s2 = should_loop(s1, len2);
+
+	while (is_s1 || is_s2)
+	{
+		if (is_s1)
+			len1++;
+		if (is_s2)
+			len2++;
+
+		is_s1 = should_loop(s1, len1);
+		is_s2 = should_loop(s2, len2);
+	}
+
+	len3 = len1 + len2 + 1;
+
+	s3 = (char *) malloc(sizeof(char) * len3);
+
+	for (i = 0, j = 0; i < len3; i++)
+		*(s3 + i) = i < len1
+			? *(s1 + i)
+			: *(s2 + j++);
+	*(s3 + i) = '\0';
+
+	return (s3);
+}
+
+/**
+ * should_loop - determine if a giving string reached a null char
+ *		  and it's value is not NULL
+ * @s: string to be checked
+ * @i: index of the current loop round
+ *
+ * Return: (1) if should keep iterating otherwise (0)
+ */
+
+short should_loop(char *s, short i)
+{
+	return (s != NULL && *(s + i) != '\0');
+}
+
