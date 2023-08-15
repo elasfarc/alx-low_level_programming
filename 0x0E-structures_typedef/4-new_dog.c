857fb1ff9de1a65121596dff5ac79f8cb054f573
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-15 02:36:54 +0000

    new_dog(..)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,7 @@
 #include "dog.h"
 
 char *_strcpy(char *);
+int _strlen(char *);
 
 /**
  * new_dog - a function that creats a new dog
@@ -28,12 +29,43 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+	dog->name = malloc(sizeof(char) * _strlen(name));
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+	}
 	dog->name = name_cp;
+
+	dog->owner = malloc(sizeof(char) * _strlen(owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+	}
 	dog->owner = owner_cp;
+
 	dog->age = age;
 
 	return (dog);
 
+}
+
+/**
+ * _strlen - determine the length of a given string
+ * @s: string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
 
 /**
@@ -45,11 +77,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 char *_strcpy(char *s)
 {
-	short len = 0, i = 0;
+	short i = 0, len;
 	char *copied;
 
-	while (*(s + len) != '\0')
-		len++;
+	len = _strlen(s);
 
 	copied = malloc((sizeof(char) * len) + 1);
 
