d122140743fe75292414fdeccd6f897608ffe2df
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-15 00:04:05 +0000

    print_dog(..)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,19 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	       return;
-	/*printf("Name: %s\nAge:(%) %f\nOwner: %s\n",
-			(d->name == NULL ? "(nil)" : d->name),
-			 d->age,
-			 (d->owner == NULL ? "(nil)" : d->owner)
-			 );*/
 	printf("Name: %s\n", (d->name == NULL ? "(nil)": d->name));
-	printf("Age: ");
-	/*printf((d->age == 0.0) ? "%s" : "%f",
-			(d->age == 0.0 ? "(nil)": d->age));*/
-	if(d->age == 0.0)
-		printf("(nill)\n");
-	else
-		printf("%f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner == NULL ? "(nil)": d->owner));	
 		
 }
