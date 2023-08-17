39480f564e9542d44fcc6b467438bfddf7683a1c
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-17 00:01:16 +0000

    print_name check if any of the passed pointers are NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  * print_name - a function that prints a name
  * @name: name to be printed.
@@ -7,6 +9,8 @@
  */
 void print_name(char *name, void(*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
 
