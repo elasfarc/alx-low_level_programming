47b598798674e4e6669330489bd1eb2e6579d40d
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-25 01:24:30 +0000

    free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the start of the list
+ *
+ * Return: always void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
