66b3a528877be2da98a0768aa4f0af9eddf5c0db
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-31 23:23:13 +0000

    Add print_binary(..)

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include "main.h"
+
+#define FIRST_ONE_BIT(x) ((x) > 0 ? (x) - 1 : 0)
+
+unsigned int get_bit(unsigned long int n, unsigned int i);
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: binary number to be printed.
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	size_t bits = sizeof(unsigned long int) * 4;
+	int i, most_left_zero;
+
+	for (i = bits - 1; i >= 0; i--)
+	{
+		if (get_bit(n, i))
+			break;
+		most_left_zero = i;
+	}
+
+	i = FIRST_ONE_BIT(most_left_zero);
+	for (; i >= 0; i--)
+		_putchar(get_bit(n, i) + '0');
+}
+
+/**
+* get_bit - returnts the bit at a given index.
+* @n: lens of search.
+* @i: the index of bit to look at
+*
+* Return: 1 if the bit at the given index is 1, 0 otherwise.
+*/
+
+unsigned int get_bit(unsigned long int n, unsigned int i)
+{
+	return ((n & (1 << i)) ? 1 : 0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 unsigned int str_len(const char *s);
 unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int n);
 
 #endif
