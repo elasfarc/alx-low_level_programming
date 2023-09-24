89b06bc45264e696293d00133707cfadbcf32a30
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-09-24 16:52:06 +0300

    Add _strchr(..)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -19,10 +19,12 @@ char *_strchr(char *s, char c)
 		return (NULL);
 
 	for (i = 0; s[i]; i++)
-		if (s[i] == c || (s[i + 1] == c && c == '\0'))
+	{
+		if (s[i] == c)
 			return (s + i);
-
-
+		if (s[i + 1] == c && c == '\0')
+			return s + i + 1;
+	}
 
 	return (NULL);
 }
