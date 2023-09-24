f5e67b58fcc40e59f81078aa6c2c8e99fff18d93
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-09-24 10:52:29 +0300

    Add _strncpy(..)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i, src_len;
+
+	if (!dest)
+		return (NULL);
+
+	for (i = 0; src[i]; i++)
+		;
+	src_len = i + 1;
+
+	for (i = 0; (i < n ); i++)
+		dest[i] = i >= src_len
+			? '\0'
+			: src[i];
+
+	return (dest);
+
+}
