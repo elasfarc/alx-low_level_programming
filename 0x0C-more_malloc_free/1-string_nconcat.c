93277476cc1953c0e36d4e18e8a718e656bc25a5
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-09-25 10:32:46 +0300

    Add string_nconcat(..) concatenates two strings.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+size_t _strlen(char *s);
+
+/**
+ * string_nconcat - Concatenate two strings, limiting the second string
+ * to 'n' bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes from s2 to concatenate.
+ *
+ * Return: A pointer to the newly allocated concatenated string,
+ * or NULL on failure.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	size_t new_len, len1, len2, i, j;
+	char *concated;
+
+	s1 = s1 ? s1 : "";
+	s2 = s2 ? s2 : "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	new_len = len1 + (n >= len2 ? len2 : n) + 1;
+
+	concated = malloc(sizeof(char) * new_len);
+	if (!concated)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concated[i] = s1[i];
+	for (j = 0; i < new_len; (i++, j++))
+		concated[i] = s2[j];
+
+	concated[new_len] = '\0';
+
+	return (concated);
+}
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string, or 0 if 's' is NULL.
+ */
+size_t _strlen(char *s)
+{
+	size_t i;
+
+	if (!s)
+		return (0);
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (i);
+}
