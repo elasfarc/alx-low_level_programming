3de65f48bcca12a93d5a959c9a0b9ea46301883f
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-11-02 21:17:47 +0200

    prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print the contents of a hash table
+ * @ht: Pointer to the hash table
+ *
+ * Description:
+ * This function prints the contents of a hash table in the following format:
+ * {index
+ *  'key': 'value', index 'key': 'value', ...
+ * }
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, atleast_one_printed = 0;
+	hash_node_t *node;
+
+	if (!ht)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			printf("\n%ld\n", i);
+
+			printf("%s'%s': '%s'",
+				atleast_one_printed ? ", " : "",
+				node->key, node->value
+			);
+			node = node->next;
+			atleast_one_printed = 1;
+		}
+	}
+	printf("}\n");
+}
